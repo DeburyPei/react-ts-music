4380de39dfcab16d6c182b376e0d0bdee01fe31d
Debury17 <svipbighead67@gmail.com>
2023-09-24 20:59:29 +0800

    了解hashRouter 初步实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Suspense } from 'react'
+import { useRoutes } from 'react-router-dom'
+import '@/App.css';
+import { HashRouter } from 'react-router-dom';
+import routes from '@/router'
 function App() {
   return (
+
     <div className="App">
-     hello
+      <h1>Hello</h1>
+        { useRoutes(routes) }
     </div>
   );
 }
