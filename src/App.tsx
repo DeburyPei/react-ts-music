d565411eca8b19fd19e1de3c44e1d3ab7ae90418
Debury17 <svipbighead67@gmail.com>
2023-09-24 21:45:26 +0800

    定义组件基本格式

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 import '@/App.css';
-import { HashRouter } from 'react-router-dom';
 import routes from '@/router'
 function App() {
   return (
