d1d6eceb6c9a2948f5c841aad89cb92046c1f846
Debury17 <svipbighead67@gmail.com>
2023-09-26 17:20:09 +0800

    使用style-components进行页面css操作

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,22 @@ import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "@/store";
 
+import './assets/css/common.css'
+import { ThemeProvider } from 'styled-components'
+import theme from "./assets/theme";
+
+
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 // eslint-disable-next-line react/no-deprecated
 root.render(
     <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <ThemeProvider theme={theme}>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </ThemeProvider>
     </Provider>
 );
