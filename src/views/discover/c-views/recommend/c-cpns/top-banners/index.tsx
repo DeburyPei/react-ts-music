0e7a09789a954f406511c49f7d1e789781fac3c3
Debury17 <svipbighead67@gmail.com>
2023-09-29 23:34:49 +0800

    recommend-banner基础映射 未完成banner轮播图部分

--- src/views/discover/c-views/recommend/c-cpns/top-banners/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banners/index.tsx
@@ -1,14 +1,21 @@
 import React, {memo} from "react";
 import type {FC,ReactNode} from "react";
-import {BannerWrapper} from "@/views/discover/c-views/recommend/c-cpns/top-banners/index";
+import {BannerWrapper} from "@/views/discover/c-views/recommend/c-cpns/top-banners/style";
+import {shallowEqualApp, useAppSelector} from "@/store";
 interface IProps {
     children?:ReactNode,
 }
 const TopBanner:FC<IProps> = () =>{
-
+    const {banners} = useAppSelector((state)=>({
+        banners:state.recommend.banners
+    }),shallowEqualApp)
     return (
         <BannerWrapper>
-          TopBanner
+            {banners.map(item=>{
+                return <div key={item.key}>
+                    {item.imageUrl}
+                </div>
+            })}
         </BannerWrapper>
     )
 }
--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -2,17 +2,22 @@ import React, {memo, useEffect} from "react";
 import type {FC,ReactNode} from "react";
 import {useAppDispatch} from "@/store";
 import {fetchBannerDataAction} from "@/views/discover/c-views/recommend/store/recommend";
+import TopBanner from "./c-cpns/top-banners";
+
+
 interface IProps {
     children?:ReactNode,
 }
 const Recommend:FC<IProps> = () =>{
+    /** 发起action(获取数据) */
     const dispatch = useAppDispatch()
     useEffect(()=>{
         dispatch(fetchBannerDataAction())
     },[])
+    /** render函数的返回jsx */
     return (
         <div>
-          Recommend
+          <TopBanner />
         </div>
     )
 }
