f6b6eded4540f734d348bb2cdb456186d624ac13
Debury17 <svipbighead67@gmail.com>
2023-10-05 16:35:19 +0800

    完成recommend榜单所有

--- src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
@@ -2,15 +2,24 @@ import React, {memo} from "react";
 import type {FC,ReactNode} from "react";
 import {RankingWrapper} from "@/views/discover/c-views/recommend/c-cpns/top-ranking/style";
 import AreaHeaderV1 from "@/components/area-header";
+import {useAppSelector} from "@/store";
+import TopRankingItem from "@/views/discover/c-views/recommend/c-cpns/top-ranking-item";
+import ranking from "@/views/discover/c-views/ranking";
 interface IProps {
     children?:ReactNode,
 }
 const TopRanking:FC<IProps> = () =>{
+    const {rankings} = useAppSelector((state)=>({
+        rankings:state.recommend.rankings
+    }))
+    console.log(rankings)
     return (
         <RankingWrapper>
             <AreaHeaderV1 title="榜单" />
             <div className="content">
-
+                {rankings.map(item=>{
+                    return <TopRankingItem key={item.id} itemData={item}/>
+                })}
             </div>
         </RankingWrapper>
     )
